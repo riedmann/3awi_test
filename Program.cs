294a6946bb9676275bd95c82ecef599e819d1a32
anderl <anderl@rocks.com>
2021-06-07 13:17:47 +0200

    minor changes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace sample2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("gye");
         }
     }
 }
